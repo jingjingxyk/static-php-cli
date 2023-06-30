f0b37df6bf2a4ba734b08ae88d542d86ff8b8b06
jingjingxyk <zonghengbaihe521@qq.com>
2023-07-01 00:09:12 +0800

    改进

--- src/SPC/builder/unix/library/ncurses.php
+++ src/SPC/builder/unix/library/ncurses.php
@@ -32,9 +32,30 @@ trait ncurses
             ->exec('make clean')
             ->exec("make -j{$this->builder->concurrency}")
             ->exec('make install');
+        shell()->exec('cp -f ' . BUILD_ROOT_PATH . '/lib/libformw.a ' . BUILD_ROOT_PATH . '/lib/libform.a');
+        shell()->exec('cp -f ' . BUILD_ROOT_PATH . '/lib/libmenuw.a ' . BUILD_ROOT_PATH . '/lib/libmenu.a');
+        shell()->exec('cp -f ' . BUILD_ROOT_PATH . '/lib/libncurses++w.a ' . BUILD_ROOT_PATH . '/lib/libncurses++.a');
         shell()->exec('cp -f ' . BUILD_ROOT_PATH . '/lib/libncursesw.a ' . BUILD_ROOT_PATH . '/lib/libncurses.a');
+        shell()->exec('cp -f ' . BUILD_ROOT_PATH . '/lib/libpanelw.a ' . BUILD_ROOT_PATH . '/lib/libpanel.a');
+        shell()->exec('cp -f ' . BUILD_ROOT_PATH . '/lib/libticw.a ' . BUILD_ROOT_PATH . '/lib/libtic.a');
+
+        shell()->exec(
+            'cp -f ' . BUILD_ROOT_PATH . '/lib/pkgconfig/formw.pc ' . BUILD_ROOT_PATH . '/lib/pkgconfig/form.pc'
+        );
+        shell()->exec(
+            'cp -f ' . BUILD_ROOT_PATH . '/lib/pkgconfig/menuw.pc ' . BUILD_ROOT_PATH . '/lib/pkgconfig/menu.pc'
+        );
+        shell()->exec(
+            'cp -f ' . BUILD_ROOT_PATH . '/lib/pkgconfig/ncurses++w.pc ' . BUILD_ROOT_PATH . '/lib/pkgconfig/ncurses++.pc'
+        );
         shell()->exec(
             'cp -f ' . BUILD_ROOT_PATH . '/lib/pkgconfig/ncursesw.pc ' . BUILD_ROOT_PATH . '/lib/pkgconfig/ncurses.pc'
+        );
+        shell()->exec(
+            'cp -f ' . BUILD_ROOT_PATH . '/lib/pkgconfig/panelw.pc ' . BUILD_ROOT_PATH . '/lib/pkgconfig/panel.pc'
+        );
+        shell()->exec(
+            'cp -f ' . BUILD_ROOT_PATH . '/lib/pkgconfig/ticw.pc ' . BUILD_ROOT_PATH . '/lib/pkgconfig/tic.pc'
         );
     }
 }
