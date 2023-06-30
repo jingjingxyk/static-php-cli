989c2fab2cf8222018d9a9dd8ef279fc95b04d1c
jingjingxyk <zonghengbaihe521@qq.com>
2023-07-01 04:25:24 +0800

    update

--- src/SPC/builder/linux/LinuxBuilder.php
+++ src/SPC/builder/linux/LinuxBuilder.php
@@ -254,7 +254,7 @@ class LinuxBuilder extends BuilderBase
             logger()->info('bloat linking');
             $extra_libs = "-Wl,--whole-archive {$extra_libs} -Wl,--no-whole-archive";
         }
-
+        $envs .= " LDFLAGS=\"{$ldflags} -static -all-static \" ";
         if (($build_target & BUILD_TARGET_CLI) === BUILD_TARGET_CLI) {
             logger()->info('building cli');
             $this->buildCli($extra_libs, $use_lld, $envs);
