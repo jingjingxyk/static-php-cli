7245ba27f4271e8218a0abf108ed44e4b5fd88cf
jingjingxyk <zonghengbaihe521@qq.com>
2024-10-09 11:22:20 +0800

    add extension fricc2

---
+++ src/SPC/builder/extension/fricc2.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SPC\builder\extension;
+
+use SPC\builder\Extension;
+use SPC\store\FileSystem;
+use SPC\util\CustomExt;
+
+#[CustomExt('fricc2')]
+class fricc2 extends Extension
+{
+    public function getUnixConfigureArg(): string
+    {
+        return '--enable-fricc2load=static';
+    }
+}
